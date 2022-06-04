cc5e1b9b519ce03ec6948a3b483c7e4e9e750eea
Zac-Jewitt <zacjewitt22@gmail.com>
2022-06-04 20:54:34 +0930

    changes

---
+++ src/assignments/controlflow/DoWhileLoop.java
@@ -0,0 +1,26 @@
+package assignments.controlflow;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int num1 = 0;
+		int num2 = 1;
+		int counter = 10;
+		String result = "";
+		
+		
+		do {
+			
+			System.out.println(num1);
+			
+			int num3 = num1 + num2;
+			num1 = num2;
+			num2 = num3;
+					
+			counter--;
+			
+		} while(counter > 0);
+		
+	}
+
+}
